07b7132243592d5c2ddca9710fd7e12b34d1e8ed
Jovana Marceta <jmarcetafedev@gmail.cm>
2025-04-09 19:01:10 +0200

    making http request

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,26 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
- 
+
 var builder = WebApplication.CreateBuilder(args);
- 
+
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     // pass connection string
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
- 
+
+// add service support for cors 
+builder.Services.AddCors();
+
 var app = builder.Build();
- 
+
 // Configure the HTTP request pipeline.
+// specify polices for middleware to provide
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
- 
+
 app.Run();
